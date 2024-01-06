5e66eb1cfe870c1e28e1f23c4704d1974f1770e2
Raveena Ingale <ingale.r@northeastern.edu>
2024-01-05 19:20:34 -0500

    Default basic auth

---
+++ src/main/java/me/raveenaingale/springsecurity/controllers/HelloController.java
@@ -0,0 +1,15 @@
+package me.raveenaingale.springsecurity.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class HelloController {
+
+    @GetMapping("/healthz")
+    public String healthz(){
+        return "Hello";
+    }
+}
