b6c8d356ef635d2096faf4491b4d2f6e5da40432
Raveena Ingale <ingale.r@northeastern.edu>
2024-01-05 20:45:15 -0500

    permit-all

--- src/main/java/me/raveenaingale/springsecurity/config/ProjectConfig.java
+++ src/main/java/me/raveenaingale/springsecurity/config/ProjectConfig.java
@@ -22,6 +22,6 @@ public class ProjectConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.httpBasic();
         http.authorizeRequests()
-           .anyRequest().authenticated();
+           .anyRequest().permitAll();
     }
 }
