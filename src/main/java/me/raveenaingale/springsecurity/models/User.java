b1b785661f72055d16239fa95a008a14a32f1579
Raveena Ingale <ingale.r@northeastern.edu>
2024-01-07 16:21:01 -0500

    creating user and security user class

---
+++ src/main/java/me/raveenaingale/springsecurity/models/User.java
@@ -0,0 +1,19 @@
+package me.raveenaingale.springsecurity.models;
+
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Data
+@Entity
+public class User  {
+
+@Id
+private int id;
+private String username;
+private String password;
+private String authority;
+
+}
--- src/main/java/me/raveenaingale/springsecurity/security/SecurityUser.java
+++ src/main/java/me/raveenaingale/springsecurity/security/SecurityUser.java
@@ -1,49 +1,52 @@
-package me.raveenaingale.springsecurity.config;
+package me.raveenaingale.springsecurity.security;
 
+import me.raveenaingale.springsecurity.models.User;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
 import java.util.List;
 
+public class SecurityUser implements UserDetails {
 
-public class DummyUser implements UserDetails {
+    private final User user;
 
-    //returns a collection with only one implementation of the GrantedAuthority interface
+    private SecurityUser (User user) {
+        this.user = user;
+    }
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return List.of(() -> "READ");
+        return List.of(()->user.getAuthority());
     }
 
     @Override
     public String getPassword() {
-        return "123";
+        return this.user.getPassword();
     }
 
     @Override
     public String getUsername() {
-        return "Raveena";
+        return this.user.getUsername();
     }
 
     @Override
     public boolean isAccountNonExpired() {
-        return true;
+        return false;
     }
 
     @Override
     public boolean isAccountNonLocked() {
-        return true;
+        return false;
     }
 
     @Override
     public boolean isCredentialsNonExpired() {
-        return true;
+        return false;
     }
 
     @Override
     public boolean isEnabled() {
-        return true ;
+        return false;
     }
-
 
 }
